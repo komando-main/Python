2a4128c55566b4b3c4220b49533c0d5e3846c6a1
komando-main <86236221+komando-main@users.noreply.github.com>
2023-05-07 03:30:07 +0900

    불 자료형

---
+++ 불 자료형.py
@@ -0,0 +1,8 @@
+a=True
+b=False
+c=1<2
+d=1>2
+e='asd'
+f=''
+g=' '
+print(a,b,c,d,bool(e),bool(f),bool(g),bool(g.replace(' ', '')),bool(0),bool(3),bool([]),bool([1,2,3]),bool(['asd']),bool({}),bool({'asd':'qwe'}),bool({'qwe'}))
