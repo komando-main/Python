7bf423d6bd91ccfab57f6d058d43734f46bf3c53
komando-main <86236221+komando-main@users.noreply.github.com>
2023-05-12 00:49:26 +0900

    python

--- module/mod_test1.py
+++ module/mod_test1.py
@@ -1,2 +1,10 @@
 from mod_test import *
+from asd.test import * #a 라는 바인드와 name() 라는 함수가 정의 되어 있다 //// from asd.test 폴더명과 파일명이 같이 써줘야 한다 구분은 (.)으로 ////
+
 print(add(1,2))
+
+
+c='이얏후'
+print(name(a))
+print(name('와웅')) #이런식으로도 사용가능하다
+print(name(c))
