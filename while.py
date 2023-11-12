e8953ae0d1f0b9d7d4fd764fb13e0c170f1a56f5
komando-main <86236221+komando-main@users.noreply.github.com>
2023-11-12 17:01:25 +0900

    update

---
+++ while.py
@@ -0,0 +1,45 @@
+# 1 에서 5까지 의 총 합 구하기
+# i = 1
+# j = 0
+# while i <= 5:
+#     j += i
+#     i+=1
+
+# print(j)
+
+# 구구단 3 단 8단 구하기
+# i = 1
+# j = 1
+# while i < 10:
+#     while i == 3 and j < 10: #중간에 브래이크를 못 시킴
+#         i+=1
+#         j+=1
+#         print(f"{i} {j}")
+#     print(f" while 1번 안  {i}")
+#     i+=1
+
+# for i in range(1, 10):
+#     for j in range (1,10):
+#         if i == 3:
+#             print(f"{i} * {j} = {i*j}")
+#     for j in range(1, 10):
+#         if i == 8:
+#             print(f"{i} * {j} = {i*j}")
+i = 1
+
+while i < 10:
+    j = 1
+    if i == 3:
+        print(f"====== {i}단 =====")
+    elif i == 8:
+        print()
+        print(f"====== {i}단 =======")
+
+    while i == 3 and j < 10:
+        print(f"{i} * {j} = {i*j}")
+        j+=1
+    while i == 8 and j < 10:
+        print(f"{i} * {j} = {i*j}")
+        j+=1
+    i+=1
+
--- while문.py
+++ while문.py
@@ -1,9 +1,4 @@
-treeHit=0
-while treeHit < 10:
-    treeHit+=1
-    print(f'나무를 {treeHit}번 찍었습니다')
-    if treeHit == 10 :
-        print('나무 넘어 갑니다')
+# 1
 
 prompt = '''
 1.Add
@@ -17,23 +12,23 @@ while numder != 4:
     numder=int(input())
 
 
-money=0
-coffee=5
-while coffee != 0:
-    if money < 300:
-        c=int(input(f'\n남은 커피{coffee}\n\n돈을 지불하세요\n현재 금액 {money}원 필요금액 300원\n투입:'))
-        money+=c
-    elif money >= 300:
-        coffee-=1
-        print(f'\n커피가 나옵니다.\n거스름 돈 {money-300}원\n')
-        money=0
+# money=0
+# coffee=5
+# while coffee != 0:
+#     if money < 300:
+#         c=int(input(f'\n남은 커피{coffee}\n\n돈을 지불하세요\n현재 금액 {money}원 필요금액 300원\n투입:'))
+#         money+=c
+#     elif money >= 300:
+#         coffee-=1
+#         print(f'\n커피가 나옵니다.\n거스름 돈 {money-300}원\n')
+#         money=0
 
-a=0
-while a < 10:
-    a+=1
-    if a % 2 == 0:
-        continue
-    print(a)
+# a=0
+# while a < 10:
+#     a+=1
+#     if a % 2 == 0:
+#         continue
+#     print(a)
 
-while True:
-    print('Ctrl+C를 눌러야 while문을 빠저나갈 수 있습니다')
+# while True:
+#     print('Ctrl+C를 눌러야 while문을 빠저나갈 수 있습니다')
